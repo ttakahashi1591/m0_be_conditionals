df42067e07cbe7a764a3a785b1ea2db0c511cfc5
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-05-16 11:29:33 -0600

    Add chili challenges

---
+++ code_challenges/anaheim_chili_challenge.rb
@@ -0,0 +1,14 @@
+## Mild
+
+#Write a Ruby program that defines a variable that stores an Integer. 
+#The program should print out the String "even" if the Integer is even, 
+#and the String "odd" if the Integer is odd.
+
+age = 32
+
+if age % 2 == 0
+    puts "even"
+else 
+    puts "odd"
+end
+
---
+++ code_challenges/ghost_pepper_chili_challenge.rb
@@ -0,0 +1,6 @@
+## Spicy
+
+=begin Write a Ruby program that prints out a String or Integer: The printed value will depend on the value 
+of an Integer. If the Integer is a multiple of 3, print "Fizz". If the Integer is a multiple of 5, 
+print "Buzz". If the Integer is a multiple of both 3 and 5, print "FizzBuzz". 
+If the Integer is not a multiple of either, print the Integer itself. =end
